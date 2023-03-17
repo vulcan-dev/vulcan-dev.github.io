d9b1ea58aac5237b5384bcc13cc4f49ba627626e
vulcan-dev <vitexuhd@gmail.com>
2023-03-17 00:23:10 +0000

    Possibly fixed it

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -11,5 +11,9 @@ export default defineNuxtConfig({
         public: {
             BACKEND_URL: process.env.NODE_ENV === "production" ? "http://168.119.249.21:3000" : "http://localhost:8080",
         }
+    },
+    target: "static",
+    router: {
+        base: "/vulcan-dev.github.io/",
     }
 })
